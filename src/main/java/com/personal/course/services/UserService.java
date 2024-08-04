09002fd9640b72d2dd201d918f7a20e1f0d2d7aa
Gabriel Veríssimo <vrssmgabriel@gmail.com>
2024-08-03 22:15:51 -0300

    Exception handling - Update

--- src/main/java/com/personal/course/services/UserService.java
+++ src/main/java/com/personal/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.personal.course.repositories.UserRepository;
 import com.personal.course.services.exception.DatabaseException;
 import com.personal.course.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -45,10 +47,15 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
